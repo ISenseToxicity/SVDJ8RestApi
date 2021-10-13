0a2672a36f9cf066dcd11cdd3b72c6161673e773
ISenseToxicity <david@davidvanderwulp.nl>
2021-10-13 16:49:13 +0200

    initial commit

---
+++ src/main/java/nl/hsleiden/svdj8/Svdj8Application.java
@@ -0,0 +1,13 @@
+package nl.hsleiden.svdj8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Svdj8Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Svdj8Application.class, args);
+	}
+
+}
---
+++ src/test/java/nl/hsleiden/svdj8/Svdj8ApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.hsleiden.svdj8;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Svdj8ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
