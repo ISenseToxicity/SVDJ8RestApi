b7372255b4bee8f3494ca40f865485e924d52380
MisterQuack <brandonplokker@live.nl>
2021-11-07 20:27:34 +0100

    Verificatie code

--- src/main/java/nl/hsleiden/svdj8/services/VerificationService.java
+++ src/main/java/nl/hsleiden/svdj8/services/VerificationService.java
@@ -6,22 +6,33 @@ import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.Random;
+
 @Service
 public class VerificationService {
 
     @Autowired
     private JavaMailSender javaMailSender;
 
-    public void createVerificationCode() {
-
+    public String createVerificationCode() {
+        String codeNumber = String.valueOf(new Random().nextInt(89999999) + 10000000);
+        StringBuilder code = new StringBuilder();
+        for (int i = 0; i < codeNumber.length(); i++) {
+            if (i == 4) {
+                code.append(" ");
+            }
+            code.append(codeNumber.charAt(i));
+        }
+        return code.toString();
     }
 
     @PostMapping
-    public void sendEmail(String email, int code) {
+    public void sendEmail(String email) {
         SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
         simpleMailMessage.setFrom("noreply@svdj.nl");
         simpleMailMessage.setTo(email);
         simpleMailMessage.setSubject("Verificatie code SVDJ admin");
+        String code = createVerificationCode();
         simpleMailMessage.setText("Gebruik deze code om u identiteit te bevestigen voor de SVDJ admin portal \n" + code + "\n \n- SVDJ");
         javaMailSender.send(simpleMailMessage);
     }
