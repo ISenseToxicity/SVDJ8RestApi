61fa184361b46d0a20e9270d62f5bd01b6a9a59b
ISenseToxicity <david@davidvanderwulp.nl>
2021-11-09 10:43:07 +0100

    made returnObject

--- src/main/java/nl/hsleiden/svdj8/controllers/TrafficController.java
+++ src/main/java/nl/hsleiden/svdj8/controllers/TrafficController.java
@@ -1,12 +1,10 @@
 package nl.hsleiden.svdj8.controllers;
 
 import nl.hsleiden.svdj8.exceptions.NoSuchElementException;
+import nl.hsleiden.svdj8.models.ReturnModel;
 import nl.hsleiden.svdj8.models.temp.Question;
 import nl.hsleiden.svdj8.models.temp.TempQuestionListMaker;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 
@@ -14,18 +12,20 @@ import java.util.ArrayList;
 @RestController
 public class TrafficController {
 
-
-    @RequestMapping(value = "/questionlist", method = RequestMethod.GET)
+    @GetMapping(value = "/questionlist")
     public Object getQuestionList(){
         TempQuestionListMaker tempQuestionListMaker = new TempQuestionListMaker();
-        return tempQuestionListMaker.makeList();
+        ReturnModel returnModel = new ReturnModel();
+        returnModel.setHttpStatus("200");
+        returnModel.setReturnObject(tempQuestionListMaker.makeList());
+        return returnModel;
     }
 
-    @RequestMapping(value = "/question/{id}")
+    @GetMapping(value = "/question/{id}")
     public Question getQuestion(@PathVariable int id){
         TempQuestionListMaker tempQuestionListMaker = new TempQuestionListMaker();
         ArrayList<Question> questions = tempQuestionListMaker.makeList().getQuestions();
-        if(questions.size() -1 < id)throw new NoSuchElementException("index out of bounds");
+        if(questions.size() -1 < id)  throw new NoSuchElementException("index out of bounds");
         return questions.get(id);
     }
 }
