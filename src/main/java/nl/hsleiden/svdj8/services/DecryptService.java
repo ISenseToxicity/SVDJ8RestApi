c7a59ddd02d88bed21a5dfbe54dadb1f616754af
Kraylor <gameking.mohm@hotmail.com>
2021-11-10 23:40:12 +0100

    Decrypt en encrypt service gemaakt. Pom aangepast voor jwt lib. Alleen de key zou in een .env moeten staan. nog onduidelijk of token voor auth is of ook om waardes door te geven.

--- src/main/java/nl/hsleiden/svdj8/services/DecryptService.java
+++ src/main/java/nl/hsleiden/svdj8/services/DecryptService.java
@@ -1,4 +1,31 @@
 package nl.hsleiden.svdj8.services;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.Jwts;
+
 public class DecryptService {
+
+    DecryptService decryptService;
+
+
+    public String decrypt(String token) {
+        byte[] key = new byte[64];
+
+        Jws<Claims> result = Jwts.parserBuilder()
+                .requireAudience("Backend")
+                .setSigningKey(key)
+                .build()
+                .parseClaimsJws(token);
+
+        return result.getBody().get("Class", String.class);
+    }
+
+    public DecryptService getInstance() {
+        if (decryptService == null) {
+            decryptService = new DecryptService();
+        }
+        return decryptService;
+    }
+
 }
--- src/main/java/nl/hsleiden/svdj8/services/EncryptService.java
+++ src/main/java/nl/hsleiden/svdj8/services/EncryptService.java
@@ -1,4 +1,35 @@
 package nl.hsleiden.svdj8.services;
 
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+
+import java.sql.Date;
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+
 public class EncryptService {
+
+    EncryptService encryptService;
+
+    public String encrypt(String json) {
+        Instant now = Instant.now();
+        byte[] thisShouldBeASecretKey = new byte[64];
+
+        return Jwts.builder()
+                .setSubject("Backend")
+                .setAudience("Frontend")
+                .claim("JsonString", json)
+                .setIssuedAt(Date.from(now))
+                .setExpiration(Date.from(now.plus(1, ChronoUnit.MINUTES)))
+                .signWith(Keys.hmacShaKeyFor(thisShouldBeASecretKey))
+                .compact();
+    }
+
+    public EncryptService getInstance() {
+        if (encryptService == null) {
+            encryptService = new EncryptService();
+        }
+        return encryptService;
+    }
+
 }
