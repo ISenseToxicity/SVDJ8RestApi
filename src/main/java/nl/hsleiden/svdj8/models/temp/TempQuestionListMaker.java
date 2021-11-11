59b10ccdf2c009900a1f263c40e87cbf409e84ea
ISenseToxicity <david@davidvanderwulp.nl>
2021-11-11 21:08:58 +0100

    removed voorbeeld. Dont know why i pushed it haha

--- src/main/java/nl/hsleiden/svdj8/models/temp/TempQuestionListMaker.java
+++ src/main/java/nl/hsleiden/svdj8/models/temp/TempQuestionListMaker.java
@@ -4,16 +4,25 @@ import java.util.ArrayList;
 
 public class TempQuestionListMaker {
     public QuestionList makeList(){
-        ArrayList<String> answershizzle = new ArrayList<>();
-        answershizzle.add("haha");
-        answershizzle.add("hehe");
-        Answer answer = new Answer("lol1","lol1",answershizzle);
-        Answer answer1 = new Answer("lol2","lol2",answershizzle);
-        ArrayList<Answer> answers = new ArrayList<>();
-        answers.add(answer);
-        answers.add(answer1);
-        Question question = new Question("fdsdsf","fmsdkjsdf",answers,"jfdkfsdjk","jfsjksfdk");
-        Question question1 = new Question("fdsgfdgfddsf","fmgfdgfdgdfsdkjsdf",answers,"jfdkgfdgfddgffsdjk","jfsgdfdgfdgfjksfdk");
+        ArrayList<String> categoryShizzle = new ArrayList<>();
+        ArrayList<String> categoryShizzle2 = new ArrayList<>();
+        categoryShizzle.add("category1");
+        categoryShizzle.add("category2");
+        categoryShizzle2.add("category3");
+        categoryShizzle2.add("category1");
+        Answer answer = new Answer("lol1","lol1",categoryShizzle);
+        Answer answer1 = new Answer("lol2","lol2",categoryShizzle2);
+        ArrayList<Answer> answers1 = new ArrayList<>();
+        Answer answer10 = new Answer("test","test",categoryShizzle2);
+        Answer answer11 = new Answer("haah","hgffgh",categoryShizzle);
+        ArrayList<Answer> answers2 = new ArrayList<>();
+        answers2.add(answer10);
+        answers2.add(answer11);
+        answers2.add(answer);
+        answers1.add(answer);
+        answers1.add(answer1);
+        Question question = new Question("fdsdsf","fmsdkjsdf",answers1,"jfdkfsdjk","jfsjksfdk");
+        Question question1 = new Question("fdsgfdgfddsf","fmgfdgfdgdfsdkjsdf",answers2,"jfdkgfdgfddgffsdjk","jfsgdfdgfdgfjksfdk");
         ArrayList<Question> questions = new ArrayList<>();
         questions.add(question);
         questions.add(question1);
