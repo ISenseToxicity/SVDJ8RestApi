249945bc289f23d9eb5258262c079a434cf946b5
Brandon Plokker <brandonplokker@live.nl>
2021-11-15 16:11:23 +0100

    DataController functies aangemaakt

--- src/main/java/nl/hsleiden/svdj8/controllers/DataController.java
+++ src/main/java/nl/hsleiden/svdj8/controllers/DataController.java
@@ -1,4 +1,39 @@
 package nl.hsleiden.svdj8.controllers;
 
+import nl.hsleiden.svdj8.models.Data;
+
+import java.util.ArrayList;
+
 public class DataController {
+
+    public Data convertToNewData(Data oldData) {
+        return null;
+    }
+
+    public Data deconstructOldData(Data oldData) {
+        return null;
+    }
+
+    public Data reconstructOldData(Data oldData) {
+        return null;
+    }
+
+    private void isNewData(Boolean isNewData) {
+
+    }
+
+    private void setDutyToDone(String done) {
+
+    }
+
+    public ArrayList<Data> getAllData() {
+        return null;
+    }
+
+    public Data getData() {
+        return null;
+    }
+
+
+
 }
