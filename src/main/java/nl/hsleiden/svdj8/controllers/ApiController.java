7986276acf4134e9742fec442c0b0abb2602b3ac
Brandon Plokker <brandonplokker@live.nl>
2021-11-15 15:45:27 +0100

    alle functies uit het klassendiagram toegevoegd

--- src/main/java/nl/hsleiden/svdj8/controllers/ApiController.java
+++ src/main/java/nl/hsleiden/svdj8/controllers/ApiController.java
@@ -1,4 +1,67 @@
 package nl.hsleiden.svdj8.controllers;
 
+import nl.hsleiden.svdj8.models.Data;
+
 public class ApiController {
+
+    private void checkAuthentication(Data data) {
+
+    }
+
+    public void deconstructData(String json) {
+
+    }
+
+    public Data returnDataStatement(Data oldData) {
+        return null;
+    }
+
+    public void getConstructedNewData(Data newData) {
+
+    }
+
+    public void chooseTable() {
+
+    }
+
+    public Data makeSubsidieStatement(Data oldData) {
+        return null;
+    }
+
+    public Data allQuestionsStatement(Data oldData) {
+        return null;
+    }
+
+    public Data createRouteStatement(Data oldData) {
+        return null;
+    }
+
+    public Data addAnswerStatement(Data oldData) {
+        return null;
+    }
+
+    public String returnQueryAnswer(Data newData) {
+        return null; //JSON
+    }
+
+    public Data questionStatement() {
+        return null;
+    }
+
+    public Data subsidieStatement() {
+        return null;
+    }
+
+    private Data answerToQuestionStatement() {
+        return null;
+    }
+
+    public Data categoryStatement() {
+        return null;
+    }
+
+    public Data setTheme(Data oldData) {
+        return null;
+    }
+
 }
