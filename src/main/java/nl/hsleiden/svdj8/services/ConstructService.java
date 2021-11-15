69ed25f584ac28908003994a5af84e1d8f8478ca
Brandon Plokker <brandonplokker@live.nl>
2021-11-15 16:14:53 +0100

    ConstructService en DeconstructService aangemaakt

--- src/main/java/nl/hsleiden/svdj8/services/ConstructService.java
+++ src/main/java/nl/hsleiden/svdj8/services/ConstructService.java
@@ -1,4 +1,13 @@
 package nl.hsleiden.svdj8.services;
 
+import nl.hsleiden.svdj8.models.Data;
+
+import java.util.Map;
+
 public class ConstructService {
+
+    public String constructNewJson(Map<String, Object> newData) {
+        return null;
+    }
+
 }
--- src/main/java/nl/hsleiden/svdj8/services/DeconstructService.java
+++ src/main/java/nl/hsleiden/svdj8/services/DeconstructService.java
@@ -1,4 +1,13 @@
 package nl.hsleiden.svdj8.services;
 
+import nl.hsleiden.svdj8.models.Data;
+
+import java.util.Map;
+
 public class DeconstructService {
+
+    public Map<String, Object> deconstructJson(String oldData) {
+        return null;
+    }
+
 }
