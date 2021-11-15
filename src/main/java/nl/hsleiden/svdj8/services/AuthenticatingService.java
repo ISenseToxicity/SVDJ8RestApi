0d146652bf8f77f7a7473d32249e4b9f0968f885
Kraylor <gameking.mohm@hotmail.com>
2021-11-15 13:14:14 +0100

    Started AuthenticateService -Rob-

--- src/main/java/nl/hsleiden/svdj8/services/AuthenticatingService.java
+++ src/main/java/nl/hsleiden/svdj8/services/AuthenticatingService.java
@@ -1,4 +1,17 @@
 package nl.hsleiden.svdj8.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class AuthenticatingService {
+
+    private final DecryptService decryptService;
+
+    @Autowired
+    public AuthenticatingService(DecryptService decryptService) {
+        this.decryptService = decryptService;
+    }
+
+
 }
